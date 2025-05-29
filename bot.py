9ebecf3c802cbb8ad1abe09f3f05a2483c70a400
Benjamin Farhi <bdfarhi@buffalo.edu>
2025-05-29 13:04:42 -0400

    uncomment email

--- bot.py
+++ bot.py
@@ -94,10 +94,10 @@ def main():
                         message_body += f"- {card}\n"
                     message_body += "\n"
 
-                # send_email(
-                #     subject='🃏 New Cards Posted on CardsHQ',
-                #     body=message_body
-                # )
+                send_email(
+                    subject='🃏 New Cards Posted on CardsHQ',
+                    body=message_body
+                )
                 print("Sent notification for new cards:\n", message_body)
 
                 save_seen_cards(seen_cards)
