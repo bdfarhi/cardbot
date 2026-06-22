f1288cebf55c847487520d2a9ce0a54253075eb0
Benjamin Farhi <143666851+bdfarhi@users.noreply.github.com>
2026-06-22 19:34:39 -0400

    Remove installation of Chromium dependencies

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import subprocess
 subprocess.run(["python", "-m", "playwright", "install", "chromium"], check=True)
-subprocess.run(["python", "-m", "playwright", "install-deps", "chromium"], check=True)
 import requests
 from bs4 import BeautifulSoup
 import smtplib
